bbc9316c7d78f8cb5b80fb00c3bc22f76d9546f6
Akbaralishaikh-devop <akbaralishaikh989@gmail.com>
2023-09-14 16:48:26 +0530

    latest commit

--- Hello.tsx
+++ Hello.tsx
@@ -27,7 +27,7 @@ const Hello: React.FC<Props> = ({
   return (
     <View style={styles.container}>
       <Text style={styles.greeting}>
-        Hello {name}
+        Assalamwalekum {name}
         {getExclamationMarks(enthusiasmLevel)}
       </Text>
       <View>
